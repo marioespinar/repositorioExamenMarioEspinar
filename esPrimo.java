9491139c5db46a1c0ba777104f89982ff85632ff
Mario <mario@gmail.com>
2023-03-01 09:52:34 +0100

    Añadida version 2 de esPrimo, version final

--- esPrimo.java
+++ esPrimo.java
@@ -1,15 +1,22 @@
 public static boolean esPrimo(int n)
 {
     int i=2;
+    if (n<1)
+    {
+        return false;
+    }
     while(i<n)
     {
-        if (n%i==0)
+        if (n%i==0){
             return false;
+            }
         i++
+
     }
 
     return true;
-
-
 }
+
+
+
 
